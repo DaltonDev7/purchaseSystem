8d6ad49230b71420affba468c79f527b22bf5c1a
DaltonDev7 <daltontejada28@gmail.com>
2023-07-30 14:36:29 -0400

    add fontawesome

--- src/assets/js/sidebar.js
+++ src/assets/js/sidebar.js
@@ -1,19 +1,4 @@
-window.onload = function(){
-    const sidebar = document.querySelector(".sidebar");
-    const closeBtn = document.querySelector("#btn");
-    const searchBtn = document.querySelector(".bx-search")
-
-    closeBtn.addEventListener("click",function(){
-        sidebar.classList.toggle("open")
-        menuBtnChange()
-    })
-
-
-    function menuBtnChange(){
-        if(sidebar.classList.contains("open")){
-            closeBtn.classList.replace("bx-menu","bx-menu-alt-right")
-        }else{
-            closeBtn.classList.replace("bx-menu-alt-right","bx-menu")
-        }
-    }
-}
+const toggler = document.querySelector(".btn");
+toggler.addEventListener("click",function(){
+    document.querySelector("#sidebar").classList.toggle("collapsed");
+});
